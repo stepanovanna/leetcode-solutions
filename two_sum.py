0d6d343279c1be64e5c7ccf168727ebdf7342d80
stepanovanna <milostep@naver.com>
2026-05-31 17:46:51 +0900

    Refactor twoSum method for clarity

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
+        seen = {} #value -> index
         for i,num in enumerate(nums):
-            answer = target - num
-            if answer in seen:
+            answer = target - num #needed value
+            if answer in seen: #check if we already saw it
                 return [seen[answer], i]
-            seen[num] = i
+            seen[num] = i  #store value with index
